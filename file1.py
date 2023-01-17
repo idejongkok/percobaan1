f99a8e6bc3cf55215536d0053f68a741196d9c8e
uno <61499460+idejongkok@users.noreply.github.com>
2023-01-17 22:55:52 +0700

    ini baru upload
    iya

---
+++ file1.py
@@ -0,0 +1,43 @@
+# print("halo bapak") # ini kbuat cetak halo bapak yaa
+# print("halo ibu")
+
+# x = 5
+# X = 6
+
+# variabelSatuDanDua = 12
+
+# variabel_satu = 1
+
+# variabel dua = 
+
+# variabel-dua = 
+
+# 1variabel = 
+
+# variabel1 =
+# var1able = 
+
+# camelCase
+# snake_case
+
+f= """
+ini
+adalah
+string  
+"""
+
+a = "satu"
+b = 'dua'
+c = '3'
+d = '4'
+
+x = 2
+y = 3
+v = 1.2
+
+# z = v+x
+# w = b - a
+
+print(f)
+
+
